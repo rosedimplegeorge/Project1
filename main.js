192dac5343e6e5207c6bb8de1d00bc5fda5e1606
rosedimple <rosedimplegeorge@gmail.com>
2018-03-27 16:54:53 -0400

    jQuery Touched

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+$(document).ready(function () {
+    alert("Do you want to play Jeopordy ? Start Playing by selecting a question in the game board after clicking OK button")
+
+    $("#bible100").click(function(){
+         alert("Here  you go Answer the Questions ")
+        
+});
+});
+
 
 
