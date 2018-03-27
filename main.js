472ed1ce2b908ddb4ea96778b766afa5dc12501c
rosedimple <rosedimplegeorge@gmail.com>
2018-03-27 18:50:26 -0400

    Added a Click event and it's showing up the modal

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ $(document).ready(function () {
     alert("Do you want to play Jeopordy ? Start Playing by selecting a question in the game board after clicking OK button")
 
     $("#bible100").click(function(){
-         alert("Here  you go Answer the Questions ")
+        //  alert("Here  you go Answer the Questions ")
         
 });
 });
 
+//Get Button Element
+var modalButton = document.getElementById('bible100');
 
+//Get modalelememt
+var modal = document.getElementById('modalQues')
 
+//Get CloseBtn Element
+var closeBtn = document.getElementById('closeBtn')
+
+//Listen for a click
+modalButton.addEventListener('click',openModal);
+
+//Function openModal
+function openModal(){
+   modal.style.display = 'block';
+}
